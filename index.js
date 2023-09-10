7f4c9aa32998204a843dc936bb99ae9db9b76349
bluegit2015 <blue_sky_escape@protonmail.com>
2023-09-10 14:29:52 -0700

    Add axios/cors to allow client to call server url

--- index.js
+++ index.js
@@ -3,18 +3,23 @@
 
 // Imports
 import express from "express";
-import bodyParser from "body-parser";
+import cors from "cors";  // Allow server from different port to connect to server port
+//import bodyParser from "body-parser";
 //import * as dbQuery from "./queries.js"; // Set database queries
 
 // Start an express application
 const expApp = express();
 const webPort = process.env.PORT || 5000;
 
+// Additional app settings
+expApp.use(cors());
+expApp.use(express.json());
+
 // User parser to prevent sql injections
-expApp.use(bodyParser.json());
-expApp.use(
-    bodyParser.urlencoded({extended: true})
-)
+// expApp.use(bodyParser.json());
+// expApp.use(
+//     bodyParser.urlencoded({extended: true})
+// )
 
 // expApp.get - start a new get Route Handler
 // Get - watch for 'get' method
@@ -22,11 +27,14 @@ expApp.use(
 // req - request param
 // res - response param
 expApp.get('/', (req, res) => {
-res.send({hi: "there"})
+    res.json({hi: "there"})
 });
 
 // Handle routes/queries
 //expApp.get('/users/:username', dbQuery.getUserByName);
+expApp.get('/message', (req, res) => {
+    res.json({note: "API works now!"});
+});
 
 // Node.js runtime on server should listen to this port, then send
 // info to express library/functions
---
+++ myreactfullclient/src/MyUrl.tsx
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const getBaseUrl = () => {
+  let url;
+  switch (process.env.NODE_ENV) {
+    case "production":
+      url = "https://fullstackserver-gyop.onrender.com";
+      break;
+    case "development":
+    default:
+      url = "http://localhost:5000";
+  }
+
+  return url;
+};
+
+export default axios.create({
+  baseURL: getBaseUrl(),
+});
